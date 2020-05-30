1fb4339fd5a68f575150790314ff118bcbce38c0
Vinicius Pascoalini <65316815+ViniciusPascoalini@users.noreply.github.com>
2020-05-30 02:17:33 -0300

    Tutorial input usuário
    dois ticos de dopamina:
    - perguntar termo de busca
    - perguntar pelo prefixo

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ function start() {
 const content = {}
 
 content.searchTerm = askAndReturnSearchTerm()
+content.prefix = askAndReturnPrefix()
 
 function askAndReturnSearchTerm(){
 return readline.question('Type a Wikipedia search term: ')    
+}
+
+function askAndReturnPrefix(){
+  const prefixes = ['who is', 'what is', 'The history of']
+  const selectedPrefixIndex = readline.keyInSelect(prefixes)
+  const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+  return selectedPrefixText 
 }
 
 console.log(content)
