11542c96968535390b7fa5d2a8c80a02f3099028
Vinicius Pascoalini <65316815+ViniciusPascoalini@users.noreply.github.com>
2020-06-28 01:42:49 -0300

    ImageMagick
    Prepara e redimenciona as imagens, cria as imagens com as sentenças e cria a Thumbnail.
    Faltou separar isso no robô de vídeo(está no robô de imagem)

--- robots/image.js
+++ robots/image.js
@@ -8,11 +8,13 @@ const googleSearchCredentials = require('../credentials/google-search.json')
 async function robot() {
     const content = state.load()
 
-    //await fetchImagesOfAllSentences(content)
-    //await downloadAllImages(content)
+    await fetchImagesOfAllSentences(content)
+    await downloadAllImages(content)
     await convertAllImages(content)
+    await creatAllSentenceImages(content)
+    await createYouTubeThumbnail()
 
-    //state.save(content)
+    state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
         for (const sentence of content.sentences) {
@@ -90,7 +92,7 @@ async function robot() {
                     .out('0')
                     .out('-background', 'white')
                     .out('-blur', '0x9')
-                    .out('-resize', `${width}x${height}`)
+                    .out('-resize', `${width}x${height}^`)
                 .out(')')
                 .out('(')
                     .out('-clone')
@@ -109,6 +111,80 @@ async function robot() {
                     }
 
                     console.log(`> Image converted: ${inputFile}`)
+                    resolve()
+                })
+        })
+    }
+
+    async function creatAllSentenceImages(content) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            await createSentenceImage(sentenceIndex, content.sentences[sentenceIndex].text)
+        }
+    }
+
+    async function createSentenceImage(sentenceIndex, sentenceText) {
+        return new Promise((resolve, reject) => {
+            const outputFile = `./content/${sentenceIndex}-sentence.png`
+
+            const templateSettings = {
+                0: {
+                    size: '1920x400',
+                    gravity: 'center'
+                },
+                1: {
+                    size: '1920x1080',
+                    gravity: 'center'
+                },
+                2: {
+                    size: '800x1080',
+                    gravity: 'center'
+                },
+                3: {
+                    size: '1920x400',
+                    gravity: 'center'
+                },
+                4: {
+                    size: '1920x1080',
+                    gravity: 'center'
+                },
+                5: {
+                    size: '800x1080',
+                    gravity: 'center'
+                },
+                6: {
+                    size: '1920x400',
+                    gravity: 'center'
+                }
+            }
+
+            gm()
+                .out('-size', templateSettings[sentenceIndex].size)
+                .out('-gravity', templateSettings[sentenceIndex].gravity)
+                .out('-background', 'transparent')
+                .out('-fill', 'white')
+                .out('-kerning', '-1')
+                .out(`caption:${sentenceText}`)
+                .write(outputFile, (error) => {
+                    if(error) {
+                        return reject(error)
+                    }
+
+                    console.log(`> Sentence created: ${outputFile}`)
+                    resolve()
+                })
+        })
+    }
+
+    async function createYouTubeThumbnail() {
+        return new Promise((resolve, reject) => {
+            gm()
+                .in('./content/0-converted.png')
+                .write('./content/youtube-thumbnail.jpg', (error) => {
+                    if (error) {
+                        return reject(error)
+                    }
+
+                    console.log('> Creating Youtube thumbnail')
                     resolve()
                 })
         })
